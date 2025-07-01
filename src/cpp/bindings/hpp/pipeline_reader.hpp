89abf57b5e44a458ed13159a96262a0ba0f3e9f6
nishi <nishita@bhakars.com>
2025-07-01 13:00:24 +0200

    feat: using json library

--- src/cpp/bindings/hpp/pipeline_reader.hpp
+++ src/cpp/bindings/hpp/pipeline_reader.hpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <map>
 #include "../operations/hpp/base_operation.hpp"
-#include "../../../include/json-develop/single_include/nlohmann/json.hpp"
+#include <nlohmann/json.hpp>
 
 /**
  * structure to hold operation configuration from JSON
